ce469295ba1005a5ebe7a2cc75ed58cfb54d4677
dinesh <dinesh.colorwhistle@gmail.com>
2026-04-28 14:53:54 +0530

    CI/CD permission issue check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,6 @@ PROJECT_DIR="/home/eduwhistle-chatbot/htdocs/chatbot.eduwhistle.com/chatbot"
 echo "🔄 Switching to app user..."
 cd $PROJECT_DIR
 
-# Ensure correct ownership (idempotent safety)
-echo "🔐 Fixing ownership..."
-chown -R $APP_USER:$APP_USER $PROJECT_DIR
-
 echo "🔄 Resetting repo..."
 sudo -u $APP_USER git reset --hard
 
